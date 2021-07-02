b7bfe2c6728a9710a2c43b468ba6cd72923552fb
Leon Ochsenfeld <ochsenfeld@hotmail.com>
2021-07-02 23:46:00 +0200

    chore: added heartrate to pairing_page

--- lib/presentation/pages/pairing/pairing_page.dart
+++ lib/presentation/pages/pairing/pairing_page.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:workout_companion_flutter/application/core/bloc/heartrate/heartrate_bloc.dart';
 import 'package:workout_companion_flutter/application/pairing/bloc/pairing_bloc.dart';
 import 'package:workout_companion_flutter/injection.dart';
 import 'package:workout_companion_flutter/presentation/routes/app_router.gr.dart';
@@ -38,13 +39,27 @@ class _PageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              // await getIt<FlutterBlue>().stopScan();
-              BlocProvider.of<PairingBloc>(context)
-                  .add(const PairingEvent.pairingStarted());
-            },
-            child: const Text("data")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  // await getIt<FlutterBlue>().stopScan();
+                  BlocProvider.of<PairingBloc>(context)
+                      .add(const PairingEvent.pairingStarted());
+                },
+                child: const Text("data")),
+            SizedBox(height: 50),
+            BlocBuilder<HeartrateBloc, HeartrateState>(
+                builder: (context, state) {
+              return state.maybeMap(connected: (e) {
+                return Text("${e.bpm} BPM");
+              }, orElse: () {
+                return Text("NO VALUE");
+              });
+            })
+          ],
+        ),
       ),
     );
   }
